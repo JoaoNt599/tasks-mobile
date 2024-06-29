5e8be6adee1489bd9fcb5f83d285d52c55d7caf5
João Vieira <netojoao599@gmail.com>
2024-06-29 14:18:47 -0300

    Accessing login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,18 @@ import { AuthService } from '../services/auth.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService, 
+    public router: Router) { 
+    this.authService.logged().subscribe(
+      data => {
+        if (data != null) {
+          this.router.navigate(['home']);
+        }
+      },
+      err => console.log(err)
+    );
+  }
 
   ngOnInit() {
   }
